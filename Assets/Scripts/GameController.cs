b6d89b7e45e07cd74989360c7d4e938f5e548c2b
ChouDoudou <chateauxanna06@gmail.com>
2024-10-10 14:28:54 +0200

    MygPendu

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    private WordManager wordManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+    
+    }
+}
---
+++ Assets/Scripts/WordManager.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WordManager : MonoBehaviour
+{
+    // Liste de mots à deviner
+    private List<string> wordList = new List<string>()
+    {
+        "aleph", "azimut", "caiman", "classe", "csharp", "developpement", "doudou", "interface",
+        "jeu", "ordinateur", "pendu", "programmation", "unity", "variable", "vortex", "whisky",
+        "wagonnet", "walkyrie", "zebre", "zen", "zenith", "zero", "zinc", "zodiaque", "zone", "zoo", "zoom"
+    };
+    
+}
